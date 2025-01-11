a154d561494d44ff5d30d87b0f41e091d175a150
market lube <marketlubes@gmail.com>
2025-01-11 14:49:05 +0530

    doubl line section responsive set

--- src/page/Section4/DoubleLineReels.jsx
+++ src/page/Section4/DoubleLineReels.jsx
@@ -7,10 +7,7 @@ export const DoubleLineReels = () => {
   const logoss = Array.from({ length: 15 }, (_, i) => `${i + 1}`);
 
   return (
-    // <div>
-    //   <p>Loved by users from forward-thinking companies</p>
     <section id="works" className="logos">
-      {/* <p className="logos-pp">Loved by users from forward-thinking companies</p> */}
       <h1 className="logos-title"> ل ا م ع أ ل ا</h1>
       <div className="logos-rot">
         <div className="logo-slide">

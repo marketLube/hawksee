9726946d33a4aba44265df51d0abf29173891ed3
market lube <marketlubes@gmail.com>
2025-01-11 12:27:16 +0530

    unhide the app.js content

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,22 @@ import { GetInTouch } from "./page/Section9/GetInTouch";
 function App() {
   return (
     <>
-      <header>{/* <Nav /> */}</header>
+      <header>
+        <Nav />
+      </header>
       <main>
-        {/* <Bird /> */}
-        {/* <Paragraph /> */}
-        {/* <Counter /> */}
-        {/* <DoubleLineReels /> */}
-        {/* <Research /> */}
-        {/* <RealStories /> */}
+        <Bird />
+        <Paragraph />
+        <Counter />
+        <DoubleLineReels />
+        <Research />
+        <RealStories />
         <Grid />
-        {/* <MeetTheTeam /> */}
+        <MeetTheTeam />
       </main>
-      <footer>{/* <GetInTouch /> */}</footer>
+      <footer>
+        <GetInTouch />
+      </footer>
     </>
   );
 }

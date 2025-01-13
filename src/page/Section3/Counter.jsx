3bb43bbe2d14b640a82072caf41bc3e2df9b5f82
market lube <marketlubes@gmail.com>
2025-01-13 15:27:34 +0530

    chnage the section of the counter and and set to them responsive

--- src/page/Section3/Counter.jsx
+++ src/page/Section3/Counter.jsx
@@ -15,51 +15,57 @@ export const Counter = () => {
 
   return (
     <div className="counterSection">
-      <h1 className="counterup">Does it Work?</h1>
-      <ScrollTrigger
-        onEnter={() => setCounterState(true)}
-        onExit={() => setCounterState(false)}
-      >
-        <div className="stats-section">
-          {counterState && (
-            <div className="stats-section__item">
-              <h2 className="stats-section__value">
-                <CountUp start={0} end={59} duration={1.5}></CountUp>
-              </h2>
-              <p className="stats-section__label">Projects Done</p>
-            </div>
-          )}
-
-          {counterState && (
-            <div className="stats-section__item">
-              <h2 className="stats-section__value">
-                <CountUp start={0} end={23} duration={1.5}></CountUp>
-              </h2>
-              <p className="stats-section__label">Happy Clients</p>
-            </div>
-          )}
-          {counterState && (
-            <div className="stats-section__item">
-              <h2 className="stats-section__value">
-                <CountUp start={0} end={12} duration={1.5}></CountUp>CR+
-              </h2>
-              <p className="stats-section__label">Business</p>
-            </div>
-          )}
-          {counterState && (
-            <div className="stats-section__item">
-              <h2 className="stats-section__value">
-                <CountUp start={0} end={96} duration={1.5}></CountUp>%
-              </h2>
-              <p className="stats-section__label">Success Rate</p>
-            </div>
-          )}
+      <div className="counter-content">
+        <div className="title-container">
+          <h1 className="counterup">Does it Work?</h1>
         </div>
-      </ScrollTrigger>
-      <div className="buttondiv">
-        <Button>
-          View Portfolio <FiArrowDownRight />
-        </Button>
+
+        <ScrollTrigger
+          onEnter={() => setCounterState(true)}
+          onExit={() => setCounterState(false)}
+        >
+          <div className="stats-section">
+            {counterState && (
+              <div className="stats-section__item">
+                <h2 className="stats-section__value">
+                  <CountUp start={0} end={59} duration={1.5}></CountUp>
+                </h2>
+                <p className="stats-section__label">Projects Done</p>
+              </div>
+            )}
+
+            {counterState && (
+              <div className="stats-section__item">
+                <h2 className="stats-section__value">
+                  <CountUp start={0} end={23} duration={1.5}></CountUp>
+                </h2>
+                <p className="stats-section__label">Happy Clients</p>
+              </div>
+            )}
+            {counterState && (
+              <div className="stats-section__item">
+                <h2 className="stats-section__value">
+                  <CountUp start={0} end={12} duration={1.5}></CountUp>CR+
+                </h2>
+                <p className="stats-section__label">Business</p>
+              </div>
+            )}
+            {counterState && (
+              <div className="stats-section__item">
+                <h2 className="stats-section__value">
+                  <CountUp start={0} end={96} duration={1.5}></CountUp>%
+                </h2>
+                <p className="stats-section__label">Success Rate</p>
+              </div>
+            )}
+          </div>
+        </ScrollTrigger>
+
+        <div className="buttondivCounter">
+          <Button className="portfolio-btn">
+            View Portfolio <FiArrowDownRight />
+          </Button>
+        </div>
       </div>
     </div>
   );

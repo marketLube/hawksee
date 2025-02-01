e13d61a8f5bab950d3155889096cb5b8cfdc1ddc
market lube <marketlubes@gmail.com>
2025-02-01 17:02:26 +0530

    change logo to cloudinary

--- src/page/Section7/Grid.jsx
+++ src/page/Section7/Grid.jsx
@@ -15,84 +15,102 @@ import Logo11 from "../../assets/logos/prime.png";
 import Logo12 from "../../assets/logos/qpets.png";
 import Logo13 from "../../assets/logos/rasha.png";
 import Logo14 from "../../assets/logos/revive.png";
-import Logo15 from "../../assets/logos/royal.png";
-import Logo16 from "../../assets/logos/sma.png";
-import Logo17 from "../../assets/logos/tanza.png";
-import Logo18 from "../../assets/logos/artha.png";
+// import Logo15 from "../../assets/logos/royal.png";
+// import Logo16 from "../../assets/logos/sma.png";
+// import Logo17 from "../../assets/logos/tanza.png";
+// import Logo18 from "../../assets/logos/artha.png";
 
 export const Grid = () => {
   const logos = [
     {
       id: 1,
-      image: Logo4,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408817/choice_tdnzpf.png",
     },
     {
       id: 2,
-      image: Logo0,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408817/kaspersky_x5svi8.png",
     },
     {
       id: 3,
-      image: Logo10,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408818/national_czua9c.png",
     },
     {
       id: 4,
-      image: Logo2,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408816/bhima_yokz8d.png",
     },
     {
       id: 5,
-      image: Logo18,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408819/artha_m26zv7.png",
     },
     {
       id: 6,
-      image: Logo6,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408818/dubai_iss9rq.png",
     },
     {
       id: 7,
-      image: Logo7,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408816/Eventia_ukj9ov.png",
     },
     {
       id: 8,
-      image: Logo8,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408819/Whitefield_tcgze8.png",
     },
     {
       id: 9,
-      image: Logo9,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408817/kudumbini_i6ze5l.png",
     },
     {
       id: 10,
-      image: Logo3,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408817/cams_gk8naf.png",
     },
     {
       id: 11,
-      image: Logo11,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408818/prime_aclynz.png",
     },
     {
       id: 12,
-      image: Logo12,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408819/qpets_wvksna.png",
     },
     {
       id: 13,
-      image: Logo13,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408820/rasha_a93wvn.png",
     },
     {
       id: 14,
-      image: Logo14,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408818/revive_amnh6g.png",
     },
     {
       id: 15,
-      image: Logo15,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408819/royal_ovydsv.png",
     },
     {
       id: 16,
-      image: Logo16,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408819/sma_kqkwod.png",
     },
     {
       id: 17,
-      image: Logo17,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408819/tanza_wrxyoq.png",
     },
     {
       id: 18,
-      image: Logo5,
+      image:
+        "https://res.cloudinary.com/ddp7f64w0/image/upload/v1738408818/docdpro_uzovxq.png",
     },
   ];
 

fcb1e10c84317e781186db6d0b7f63675e176b65
market lube <marketlubes@gmail.com>
2025-02-01 16:28:03 +0530

    responsive set up

--- src/page/Service/LeftScroll.jsx
+++ src/page/Service/LeftScroll.jsx
@@ -39,9 +39,7 @@ function LeftScroll({ content }) {
           <span>{content?.title}</span>
         </div>
         {/* <div className="about-para-container"> */}
-        {/* <button className="clickbtn" onClick={handleRegisterNow}>
-          Register Now
-        </button> */}
+
         <p className="about-para-container-right">{content?.description}</p>
       </div>
       {/* </div> */}

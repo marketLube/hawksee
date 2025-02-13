1092b82e843d55c00a55cb11bc2270ed35ce082d
marketLube <marketlubes@gmail.com>
2025-02-14 02:17:35 +0530

    corrected code even in iphone

--- src/page/Section1/BirdMobo.jsx
+++ src/page/Section1/BirdMobo.jsx
@@ -25,8 +25,7 @@ export const BirdMobo = ({ isTesterHundered, isTesterVisible, paraInView }) => {
           transform: "scale(1) translate(0, 0)",
           transition: "all .5s ease-in-out",
         });
-        // const header = document.getElementById("header");
-        // header.scrollIntoView();
+
         window.scrollTo({ top: 0 });
       }
     }
